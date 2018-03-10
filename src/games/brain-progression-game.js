31cec1661b01b2c905019983cf830aadcdd31e41
moisey <moisvolod@gmail.com>
2018-03-10 23:44:52 +0300

    added new directory and new file

---
+++ src/games/brain-progression-game.js
@@ -0,0 +1,28 @@
+import { cons } from 'hexlet-pairs';
+import gameInterface from '..';
+import { getRandom } from '../libs';
+
+const gameHeadline = 'What number is missing in this progression?';
+
+const createProgression = () => {
+  let number = getRandom();
+  const step = getRandom(1, 10);
+  const digits = [number];
+  for (let i = 0; i < 9; i += 1) {
+    digits.push(number += step);
+  }
+  return digits;
+};
+
+const getRightAnswer = () => {
+  const quest = getRandom(1, 10);
+  const expression = createProgression();
+  const correctAnswer = expression[quest];
+  expression[quest] = '..';
+  const question = `${expression}`;
+  return cons(question, correctAnswer);
+};
+
+const playProgressionGame = () => gameInterface(gameHeadline, getRightAnswer);
+
+export default playProgressionGame;
