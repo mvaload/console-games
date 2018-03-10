f17d04d46c6d489442a907212b90597e0b6bd545
moisey <moisvolod@gmail.com>
2018-03-11 01:55:39 +0300

    updated half function

--- src/games/brain-calc-game.js
+++ src/games/brain-calc-game.js
@@ -1,36 +1,25 @@
-import { cons, car, cdr } from 'hexlet-pairs';
+import { cons } from 'hexlet-pairs';
 import gameInterface from '..';
-import { getRandomItem, getRandom } from '../libs';
-
-const signsArray = ['+', '-', '*'];
-
-const expectedAnswer = (num1, num2, sign) => {
-  switch (String(sign)) {
-    case '+':
-      return Number(num1) + Number(num2);
-    case '-':
-      return Number(num1) - Number(num2);
-    case '*':
-      return Number(num1) * Number(num2);
-    default:
-      return false;
-  }
-};
+import { getRandom } from '../func';
 
 const gameHeadline = 'What is the result of the expression?';
 
 const getRightAnswer = () => {
-  const pairOfNumbers = cons(getRandom(), getRandom());
-  const expression = cons(pairOfNumbers, getRandomItem(signsArray));
-  const num1 = car(car(expression));
-  const num2 = cdr(car(expression));
-  const sign = cdr(expression);
-  const correctAnswer = expectedAnswer(num1, num2, sign);
-  const question = `${num1} ${sign} ${num2}`;
-  return cons(question, correctAnswer);
+  const operators = ['+', '-', '*'];
+  const currentOperator = operators[getRandom(0, operators.length - 1)];
+  const a = getRandom(1, 20);
+  const b = getRandom(1, 20);
+
+  const expectedAnswer = (operator) => {
+    if (operator === '+') return a + b;
+    else if (operator === '-') return a - b;
+    return a * b;
+  };
+
+  const question = `${a} ${currentOperator} ${b}`;
+  const correctAnswer = String(expectedAnswer(currentOperator));
+  
+  return cons (question, correctAnswer);
 };
-
-
 const playCalcGame = () => gameInterface(gameHeadline, getRightAnswer);
-
 export default playCalcGame;
